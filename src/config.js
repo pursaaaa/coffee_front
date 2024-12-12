cd78fe2cb4506d8c071575ae8dcef4ddd0d0bd74
pursa <pung2319@hotmail.com>
2024-12-12 22:52:23 +0700

    update database url for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiPath: process.env.REACT_APP_API_URL || 'https://coffee-api-9lij.onrender.com',
+    apiPath: process.env.REACT_APP_API_URL || 'http://localhost:3001',
     headers: () => {
         return {
             headers: {
